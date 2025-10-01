0ea35c221499e2bd5179a16d8be3d3732388e2e7
Kozrerackij <dimaxgroup@inbox.ru>
2025-10-01 22:05:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class data_adress {
+private:
+	string city;
+	string street;
+	int house;
+	int flat;
+public:
+	data_adress() : city(""), street(""), house(0), flat(0) {};
+	
+	void set_data(const string& c, const string& s, int& h, int& f) {
+		city = c;
+		street = s;
+		house = h;
+		flat = f;
+	}
+
+	void print() {
+		cout << city << ", " << street << ", дом " << house << ", кв. " << flat << endl;
+	}
+	
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int size;
+	ifstream file("in.txt");
+	if (!file.is_open()) {
+		cerr << "Неудалось открыть файл!" << endl;
+		return 1;
+	}
+	file >> size;
+
+	data_adress* adress = new data_adress[size];
+	for (int i{}; i < size; i++) {
+		string city, street;
+		int house, flat;
+		file >> city >> street >> house >> flat;
+			
+		adress[i].set_data(city, street, house, flat);
+	}
+	for (int i = 0; i < size; i++) {
+		adress[i].print();
+	}
+
+	delete[] adress;
+	return EXIT_SUCCESS;
+}
